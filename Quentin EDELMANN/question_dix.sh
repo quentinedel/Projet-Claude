257307870e3eea28311bf5c23dd73e2d06a78aa9
quentinedel <55128450+quentinedel@users.noreply.github.com>
2019-09-19 10:40:48 +0200

    Update question_dix.sh

--- Quentin EDELMANN/question_dix.sh
+++ Quentin EDELMANN/question_dix.sh
@@ -3,6 +3,9 @@
 
 #Ce sript affiche le nombre d'IP configuré sur un machine Linux
 
-#J'extrait de la commande "ip a" les adresses ip configurés puis je les comptes
+#affiche les IPs configurés
+ip a | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])'
+
+#Puis je les comptes
 ip a | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' | wc -l
 
