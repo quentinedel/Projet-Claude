d3f85e9ae779d5ccfdff17fbe99b8270ab9bf90b
quentin71380 <55533207+quentin71380@users.noreply.github.com>
2019-09-19 10:45:47 +0200

    Create devoir.sh

---
+++ Quentin_Toroborelli/devoir.sh
@@ -0,0 +1,12 @@
+#!/binsh
+#recuperation des ip
+search=$(hostname -I)
+#on définis le compteur
+ctr=0
+#chaque espace = une address ip
+for i in 'echo $search |tr " " " "'
+do
+#+1 pour chaque ip
+  ctr=$(($ctr +1))
+done
+echo "cette machine possède "$ctr"asresse ip définies"
